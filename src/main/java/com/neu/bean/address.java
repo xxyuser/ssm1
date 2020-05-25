6579f0df754e8da2f8bd24bfb9cf9ead2c4feb33
xxyuser <1350472631@qq.com>
2020-05-25 09:55:20 +0800

    地址类的描述

--- src/main/java/com/neu/bean/address.java
+++ src/main/java/com/neu/bean/address.java
@@ -1,4 +1,31 @@
 package com.neu.bean;
 
 public class address {
+    private String addressId;
+    private String addressName;
+    private String addressNaote;  //备注
+
+    public String getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(String addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getAddressName() {
+        return addressName;
+    }
+
+    public void setAddressName(String addressName) {
+        this.addressName = addressName;
+    }
+
+    public String getAddressNaote() {
+        return addressNaote;
+    }
+
+    public void setAddressNaote(String addressNaote) {
+        this.addressNaote = addressNaote;
+    }
 }
